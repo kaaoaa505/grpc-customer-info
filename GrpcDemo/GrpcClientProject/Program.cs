59554994b8d558ddf3fd427b87e79655e16fdea2
KhaledAllam <kaaoaa505@gmail.com>
2021-08-16 14:15:10 +0300

    two proto with grpc

---
+++ GrpcDemo/GrpcClientProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Net.Client;
+using GrpcServerProject;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClientProject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Client Start.");
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            await sayHello(channel);
+
+            await getCustomerInfo(channel, 1);
+            await getCustomerInfo(channel, 2);
+
+            Console.WriteLine("Client End.");
+
+            Console.ReadLine();
+        }
+
+        private static async Task sayHello(GrpcChannel channel)
+        {
+            var input = new HelloRequest { Name = "Khaled" };
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+        }
+
+        private static async Task getCustomerInfo(GrpcChannel channel, int userId)
+        {
+            var client = new Customer.CustomerClient(channel);
+
+            var model = new CustomerLookupModel { UserId = userId };
+
+            var reply = await client.GetCustomerInfoAsync(model);
+
+            Console.WriteLine(reply.FirstName + " " + reply.LastName );
+        }
+    }
+}
---
+++ GrpcDemo/GrpcServerProject/Services/CustomersService.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServerProject.Services
+{
+    public class CustomersService: Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if(request.UserId == 1)
+            {
+                output.FirstName = "Khaled";
+                output.LastName = "Abdalla";
+            }
+            else
+            {
+                output.FirstName = "Unkown";
+                output.LastName = "User";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
